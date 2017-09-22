54ebc65bd633994e9162b3f406299acc68eda687
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-22 18:24:27 -0400

    all books show in text, json and csv

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,25 @@
 class BooksController < ApplicationController
 
+ def index
+   @books = Book.all
 
+   respond_to do |format|
+     format.html
+     format.text
+
+     format.json do
+       render json: @books.map { |book|
+       {Title: book.title,
+        Author: book.author,
+        Read?: book.already_read
+        }
+      }
+
+    end
+    
+
+     format.csv { render plain:" #{Book.generate_csv(@books)}"}
+
+end
+end
 end
